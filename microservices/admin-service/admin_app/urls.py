db60a676076d3e8e3cae29ff3e794687e814c600
Mr Ange <161043571+Ndong-Ange@users.noreply.github.com>
2025-05-20 15:27:21 +0100

    Deleted urls.py

--- microservices/admin-service/admin_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.admin_login, name='admin_login'),
-    path('setup', views.create_default_admin, name='create_default_admin'),
-    path('me', views.get_admin_info, name='get_admin_info'),
-    path('activities', views.get_seller_activities, name='get_seller_activities'),
-]
