9f7cb7cc8df58f88014aa2a7c64a7a889d1ae688
Mr Ange <161043571+Ndong-Ange@users.noreply.github.com>
2025-05-20 15:25:06 +0100

    Add admin edit page and functionality

--- microservices/admin-service/admin_app/urls.py
+++ microservices/admin-service/admin_app/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('setup', views.create_default_admin, name='create_default_admin'),
     path('me', views.get_admin_info, name='get_admin_info'),
     path('activities', views.get_seller_activities, name='get_seller_activities'),
-    path('sellers/<str:seller_id>', views.manage_seller, name='manage_seller'),
 ]
